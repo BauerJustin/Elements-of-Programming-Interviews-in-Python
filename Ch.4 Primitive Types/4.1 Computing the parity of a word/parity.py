3ffb4fc70e47d270ff7184adb1cd8d81acaaf124
justinbauer <jbauer@informonics.com>
2020-07-14 17:56:27 -0600

    Brute force solution to parity

---
+++ Ch.4 Primitive Types/4.1 Computing the parity of a word/parity.py
@@ -0,0 +1,17 @@
+# code
+def parity(word):
+    num = 0
+    while word > 0:
+        num += word & 1
+        word >>= 1
+    return num % 2 != 0
+
+# tests
+import unittest
+
+class SimpleTest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(parity(1101), 1)
+        self.assertEqual(parity(10001000), 0)
+
+unittest.main()
