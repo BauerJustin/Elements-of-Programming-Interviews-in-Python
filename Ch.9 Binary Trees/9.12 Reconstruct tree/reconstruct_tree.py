8223d9f1358a3ebfa500ea36a04e0aaeb1706e6c
BauerJustin <justin.bauer@mail.utoronto.ca>
2020-08-27 16:29:10 -0600

    Reconstruct tree solution

---
+++ Ch.9 Binary Trees/9.12 Reconstruct tree/reconstruct_tree.py
@@ -0,0 +1,83 @@
+#code
+def reconstruct_tree(inorder, preorder):
+
+    def add_to_tree(tree, node):
+        if tree is None:
+            tree = node
+        else:
+            if tree.data < node.data:
+                if tree.right is None:
+                    tree.right = node
+                else:
+                    add_to_tree(tree.right, node)
+            else:
+                if tree.left is None:
+                    tree.left = node
+                else:
+                    add_to_tree(tree.left, node)
+        return tree
+
+    tree = None
+    for i in range(len(preorder)):
+        tree = add_to_tree(tree, BinaryTreeNode(preorder[i]))
+    return tree
+
+# Binary tree base code
+class BinaryTreeNode:
+    def __init__(self,data=None,left=None,right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.data
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.data
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.data
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.data
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+# Test
+reconstruct_tree([1,2,3,4,5,6,7,8,9], [5,2,1,4,3,6,7,9,8]).display()
